3690fec990a473d6b68ab918f06375f5e18203d9
Adambuntingg <156615419+Adambuntingg@users.noreply.github.com>
2024-12-31 16:11:27 +0000

    Rename provider.tsx to Provider.tsx and update imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,8 @@ import { cn } from "@/lib/utils"
 import { Metadata } from 'next'
 import { ClerkProvider } from '@clerk/nextjs'
 import { dark } from '@clerk/themes'
-import Provider from "./provider"
+import Provider from "./Provider"
+
 
 const fontSans = FontSans({
   subsets: ["latin"],
